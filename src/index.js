6ad9a447c125cfa0ff7fb14aa9cdb8afebc85f9c
mgDentist <gmi1987@gmail.com>
2023-05-18 12:28:52 +0400

    add full styles to index.html + js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ import 'swiper/scss/pagination';
 import './index.scss';
 import { slidersInit } from './modules/slider';
 import { videoBackgroundInit } from './modules/videoBackground';
-
+import { menuControl } from './modules/menuControl';
+import { locationHover } from './modules/locationHover';
 
 videoBackgroundInit();
+menuControl();
+locationHover();
 
 slidersInit('.about__slider', {
     pagination: {
         el: '.about__slider-pagination',
     }
+});
+
+const careerImageItems = document.querySelectorAll('.career__image-item');
+
+careerImageItems.forEach((item, i) => {
+    item.classList.add(`career__image-item--${i % 2 ? 'even' : 'odd'}`)
 })
 
 slidersInit('.career__slider', {
@@ -20,7 +29,7 @@ slidersInit('.career__slider', {
         el: '.career__slider-pagination',
     },
     breakpoints: {
-        768: {
+        576: {
             slidesPerView: 'auto',
             spaceBetween: 20,
             pagination: false,
@@ -36,4 +45,4 @@ slidersInit('.career__slider', {
             pagination: false,
         },
     }
-})
+});
